db6c392790a25f3cf02762ffc10e620951c5bfff
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-15 07:18:29 +0100

    Add $file

---
+++ JavaScript/DOM_Elements.js
@@ -0,0 +1,12 @@
+// Finding Elements by Id
+const eleId = document.getElementById("intro");
+
+// Finding Elements by Tagname
+const eleTagName = document.getElementsByTagName("a");
+
+// Finding Elements by Class name
+const eleClass = document.getElementsByClassName("logo");
+
+//Finding Elements Using CSS selectors
+const eleCSS = document.querySelectorAll("p.logo");
+
