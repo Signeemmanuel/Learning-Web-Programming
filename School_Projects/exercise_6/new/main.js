533b4db96ece26199d2b86572d96f036b71a0b69
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-15 07:18:30 +0100

    Add $file

---
+++ School_Projects/exercise_6/new/main.js
@@ -0,0 +1,4 @@
+let firstName = prompt("Please Enter your First Name");
+let lastName = prompt("Please Enter Last Name");
+
+document.getElementById("greet").innerHTML = "Hello " + firstName + " " + lastName;
